d1bcc2115d064fbd82eda7bc65e0c4db9101c94f
pedroclavero <pedro_vitor1234@hotmail.com>
2021-11-22 07:45:08 -0300

    Classes com exemplos de herança e alguns desafios

---
+++ src/oo/heranca/desafio/Ferrari.java
@@ -0,0 +1,53 @@
+package oo.heranca.desafio;
+
+public class Ferrari extends Carro implements Esportivo {
+
+    private boolean ligarTurbo = false;
+    private boolean ligarAr = false;
+
+    public Ferrari() {
+        this(315);
+    }
+
+    public Ferrari(int velocidadeMaxima) {
+        super(velocidadeMaxima);
+        setDelta(15);
+    }
+
+    @Override
+    public void ligarTurbo() {
+        ligarTurbo = true;
+    }
+
+    @Override
+    public void desligarTurbo() {
+        ligarTurbo = false;
+    }
+
+    public void ligarAr() {
+        ligarAr = true;
+    }
+
+
+    public void desligarAr() {
+        ligarAr = false;
+    }
+
+    @Override
+    public int getDelta() {
+        if(ligarTurbo && !ligarAr) {
+            return 35;
+        } else if(ligarTurbo && ligarAr) {
+            return 30;
+        } else if(!ligarTurbo && !ligarAr) {
+            return 20;
+        } else {
+            return 15;
+        }
+    }
+
+//	@Override
+//	public void acelerar() {
+//		super.acelerar();
+//	}
+}
